11abacb3d04482d299d4ae4b20e63c66ee762ef7
Rory Armstrong <rarmstrong@atlassian.com>
2022-08-04 13:41:20 +0100

    Reducing test files to a single test for demoing

--- src/isme/database.itest.ts
+++ src/isme/database.itest.ts
@@ -1,6 +1,4 @@
 import {getDatabase} from "./database";
-import {IsMe} from "./isme";
-import {MongoError} from "mongodb";
 
 const ME = {
     name: "me"
@@ -15,67 +13,11 @@ beforeEach(tearDownDatabase);
 
 describe("Get All", () => {
     it("Empty List", async () => {
-        await expectIsMeRecordsToBe([]);
+        const allOfMe = await database.getAll();
+        expect(allOfMe).toStrictEqual([]);
     });
 });
-
-describe("Add", () => {
-    it("Add me", async () => {
-        await add().then(result =>
-            expect(result).toStrictEqual(ME)
-        );
-        await expectIsMeRecordsToBe([ME]);
-    });
-
-    it("Duplicate Add me", async () => {
-        await add();
-        await add().catch(err => {
-            expect(err).toBeInstanceOf(MongoError);
-        });
-    });
-});
-
-describe("Remove", () => {
-    it("Remove non-existing record", async () => {
-        await remove();
-    });
-
-    it("Remove existing record", async () => {
-        await add();
-        await remove();
-        await expectIsMeRecordsToBe([]);
-    });
-});
-
-describe("Get", () => {
-    it("Get non-existing record", async () => {
-        await get().then(result =>
-            expect(result).toBeUndefined()
-        );
-    });
-
-    it("Get existing record", async () => {
-        await add();
-        await get().then(result =>
-            expect(result).toStrictEqual(ME)
-        );
-    });
-});
-
-async function expectIsMeRecordsToBe(isMeRecords: IsMe[]) {
-    const allOfMe = await database.getAll();
-    expect(allOfMe).toStrictEqual(isMeRecords);
-}
-
-async function add(): Promise<IsMe> {
-    return database.add(ME)
-        .then(() => ME);
-}
 
 async function remove() {
     await database.remove(ME);
-}
-
-async function get(): Promise<IsMe> {
-    return database.get(ME);
 }
--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,4 +1,4 @@
-import {test, expect, Page} from "@playwright/test";
+import {test, expect} from "@playwright/test";
 import http from "http";
 import app from "../src/app";
 import {getDatabase} from "../src/isme/database";
@@ -28,86 +28,9 @@ test.afterAll(() => {
 test.beforeEach(tearDownDatabase);
 
 test("Correct Title", async ({ page }) => {
-    await goToHome(page);
-    await expectTitleToBeCorrect(page);
-});
-
-test("Correct Headline Header", async ({ page }) => {
-    await goToHome(page);
-    await expectHeadlineHeaderToBeCorrect(page);
-});
-
-test("Add User", async ({ page }) => {
-    await goToHome(page);
-    const name = USER_1.name;
-
-    await addUser(page, name);
-
-    await expectNameToBeVisible(page, name);
-});
-
-test("Add multiple Users", async ({ page }) => {
-    await goToHome(page);
-    const name1 = USER_1.name;
-    const name2 = USER_2.name;
-
-    await addUser(page, name1);
-    await addUser(page, name2);
-
-    await expectNameToBeVisible(page, name1);
-    await expectNameToBeVisible(page, name2);
-});
-
-test("Remove user", async ({ page }) => {
-    await goToHome(page);
-    const name = USER_1.name;
-
-    await addUser(page, name);
-    await removeUser(page, name);
-
-    await expectNameToNotBePresent(page, name);
-});
-
-async function goToHome(page: Page) {
     await page.goto("http://localhost:1000/");
-}
-
-async function expectTitleToBeCorrect(page: Page) {
     await expect(page).toHaveTitle("Is Me");
-}
-
-async function expectHeadlineHeaderToBeCorrect(page: Page) {
-    const header = page.locator("text=Folks who are Ryan Gosling");
-    await expect(header).toBeVisible();
-}
-
-async function addUser(page: Page, name: string) {
-    await inputName(page, name);
-    await submitName(page);
-}
-
-async function inputName(page: Page, name: string) {
-    const thisIsMeInput = page.locator("id=this-is-me-name");
-    await thisIsMeInput.fill(name);
-}
-
-async function submitName(page: Page) {
-    const thisIsMeButton = page.locator("id=this-is-me-button");
-    await thisIsMeButton.click();
-}
-
-async function removeUser(page: Page, name: string) {
-    const thisIsMeButton = page.locator(`id=remove-me-${name}`);
-    await thisIsMeButton.click();
-}
-
-async function expectNameToBeVisible(page: Page, name: string) {
-    await expect(page.locator(`id=name-${name}`)).toBeVisible({ timeout: 10000 });
-}
-
-async function expectNameToNotBePresent(page: Page, name: string) {
-    await expect(page.locator(`id=name-${name}`)).toHaveCount(0, { timeout: 10000 });
-}
+});
 
 async function remove() {
     await database.remove(USER_1);
